d365230b022d3e35403987aa2034cb163f686905
felippodev <coelho.luizfelippo@gmail.com>
2023-08-26 17:44:20 -0300

    Feat: celery setup

--- backend/digitalsys/settings.py
+++ backend/digitalsys/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# CELERY CONFIG
+CELERY_BROKER_URL = config('CELERY_BROKER_URL')
+CELERY_RESULT_BACKEND = config('CELERY_RESULT_BACKEND')
 
 # Application definition
 
